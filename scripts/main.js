1ad45612d4f9ab5517aa4d8882b320e9f2288ed1
dhruv1067 <dhruv67d@gmail.com>
2025-10-06 13:57:48 +0530

    main js file done

---
+++ scripts/main.js
@@ -0,0 +1,76 @@
+/* Variable naming convention: <object>_<action>_<objectname>; */
+
+var inp_as = document.getElementById('a_size'), array_size=inp_as.values;
+var inp_gen = document.getElementById('a_generate');
+var inp_aspeed = document.getElementById('a_speed');
+
+var buts_algos = document.querySelectorAll(".algos button");
+
+var div_sizes=[];
+var divs=[];
+var margin_size;
+var cont= document.getElementById("array_container");
+cont.style="flex-direction:row";
+
+inp_gen.addEventListener("click",generate_array);
+inp_as.addEventListener("input",update_array_size);
+
+function generate_array()
+{
+    cont.innerHTML="";
+
+    for(var i=0; i<array_size;i++){
+        div_sizes[i]=Math.floor(Math.random()*0.5*(inp_as.max - inp_as.min)) + 10;
+        divs[i]=document.createElement("div");
+        cont.appendChild(divs[i]);
+        margin_size=0.1;
+        divs[i].style="margin:0%" + margin_size + "%; background-color:blue; width:" + (100/array_size+(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+    }
+}
+
+function update_array_size(){
+    array_size=inp_as.value;
+    generate_array();
+}
+
+window.onload = update_array_size();
+
+for(var i=0; buts_algos.length;i++){
+    buts_algos[i].addEventListener("click",runalgo);
+}
+
+function disable_buttons()
+{
+    for(var i=0; i<buts_algos.length;i++){
+        buts_algos[i].classList=[];
+        buts_algos[i].classList.add("but_locked");
+
+        buts_algos[i].disabled=true;
+        inp_as.disabled=true;
+        inp_gen.disabled=true;
+        inp_aspeed.disabled=true;
+    }
+}
+
+function runalgo()
+{
+    disable_buttons();
+
+    this.classList.add("but_selected");
+
+    switch(this.innerHTML)
+    {
+        case "Bubble":Bubble();
+        break;
+        case "Selection":Selection_sort();
+        break;
+        case "Insertion": Insertion();
+        break;
+        case "Merge": Merge();
+        break;
+        case "Quick": Queue();
+        break;
+        case "Heap": Heap();
+        break;
+    }
+}
